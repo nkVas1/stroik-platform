53405d694c35debaf792ee91d810ae4d823325b2
nkVas1 <tajnatajnov@gmail.com>
2026-05-02 10:13:32 +0300

    chore(backend): add reviews to routers __init__.py

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,3 +1,3 @@
-from app.routers import chat, users, projects, auth
+from app.routers import chat, users, projects, auth, reviews
 
-__all__ = ["chat", "users", "projects", "auth"]
+__all__ = ["chat", "users", "projects", "auth", "reviews"]
